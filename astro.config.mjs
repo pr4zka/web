2bfc94858eb53c7526f9c8f55bfd2d7418799132
Ivan Martinez <pr4zka@gmail.com>
2026-04-03 15:48:48 -0300

    set custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://pr4zka.github.io',
-  base: '/web',
+  site: 'https://pr4zka.online',
   output: 'static',
 });
