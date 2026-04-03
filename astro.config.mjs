b3ae343c67fe032c1b77093aebe27f38d6789f8b
Ivan Martinez <pr4zka@gmail.com>
2026-04-03 15:35:14 -0300

    configure site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,8 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://pr4zka.github.io',
+  base: '/web',
+  output: 'static',
+});
