8dda5ce3d2006879ec620a5af39194603ea61b65
Ivan Martinez <pr4zka@gmail.com>
2026-04-03 15:55:41 -0300

    use github pages default url

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://pr4zka.online',
+  site: 'https://pr4zka.github.io',
+  base: '/web',
   output: 'static',
 });
