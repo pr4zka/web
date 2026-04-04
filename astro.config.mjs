0c63395cf5c148ef1816a0b79b7c350d2acbc59e
Ivan Martinez <pr4zka@gmail.com>
2026-04-04 00:12:05 -0300

    fix: correct base path in Astro configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://pr4zka.github.io',
-  base: '/web',
+  base: '/web/',
   output: 'static',
   integrations: [sitemap()],
   prefetch: true,
